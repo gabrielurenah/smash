65c231d0ea45a3964c4b68c8a1a387da61d8528b
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-03-19 19:59:04 -0400

    adds connection with mongo port

--- characters-service/src/index.js
+++ characters-service/src/index.js
@@ -1,3 +1,20 @@
-import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Mongo => connection Succesfull👌");
+  })
+  .catch(err => {
+    console.log("ERROR⚠️", err);
+  });
 
 console.log("werking");
