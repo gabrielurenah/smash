ca805d42b8303cf594d49d94980cd27dfc959b5e
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-03-20 03:43:58 -0400

    removes tests

--- characters-service/src/index.js
+++ characters-service/src/index.js
@@ -2,12 +2,12 @@ import Hapi from '@hapi/hapi';
 import { PORT } from './config/dotenv';
 import connectToDB from './services/mongo';
 
-connectToDB();
-
 const server = Hapi.server({
   port: PORT,
   host: '0.0.0.0',
 });
+
+connectToDB();
 
 server.route({
   method: 'GET',
@@ -15,18 +15,15 @@ server.route({
   handler: () => 'Hello World!',
 });
 
-exports.start = async () => {
+const init = async () => {
   await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-  return server;
-};
-
-exports.init = async () => {
-  await server.initialize();
-  return server;
 };
 
 process.on('unhandledRejection', err => {
   console.log(err);
   process.exit(1);
 });
+
+init();
+
+export default server;
