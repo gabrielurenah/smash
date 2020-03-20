0c54e14dd70a87cd69d22387bbf2a47a19e080bc
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-03-20 00:54:55 -0400

    adds connection with postgres

---
+++ players-service/sequelize/migrations/202003192021_create_players.js
@@ -0,0 +1,38 @@
+module.exports.up = (queryInterface, DataTypes) => {
+  return queryInterface.createTable(
+    "players",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      fullName: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      playerTag: {
+        allowNull: false,
+        type: DataTypes.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE
+      }
+    },
+    {
+      charset: "utf8"
+    }
+  );
+};
+
+module.exports.down = queryInterface => queryInterface.dropTable("players");
