2af4e31345b918ed31ce93b60a344991657b4f36
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-04-02 13:00:37 -0400

    changes "schemas" for "data"

--- characters-service/src/rest/utils/list.js
+++ characters-service/src/rest/utils/list.js
@@ -3,9 +3,9 @@ import { INTERNAL_SERVER_ERROR, OK } from '../../config/statusCodes';
 
 export default async function({ h }, Model) {
   const { name } = Model.collection;
-  const [err, schemas] = await wrapper(Model.find());
+  const [err, data] = await wrapper(Model.find());
 
   return err
     ? h.response({ err }).code(INTERNAL_SERVER_ERROR)
-    : h.response({ [name]: schemas }).code(OK);
+    : h.response({ [name]: data }).code(OK);
 }
