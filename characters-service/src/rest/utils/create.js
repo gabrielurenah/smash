381df35241407f1dbf33b937e54185bc855a63de
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-04-02 13:08:47 -0400

    adds singular name property

--- characters-service/src/rest/utils/create.js
+++ characters-service/src/rest/utils/create.js
@@ -2,6 +2,9 @@ import wrapper from './async';
 import { INTERNAL_SERVER_ERROR, CREATED } from '../../config/statusCodes';
 
 export default async function({ request, h }, Model) {
+  //make model name singular
+  const name = Model.collection.name.slice(0, -1);
+
   const data = new Model(request.payload);
   const [error, savedData] = await wrapper(data.save());
 
