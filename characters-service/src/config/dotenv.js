bcb5c24ca7346f377ad2f2821aead1cb99dff0aa
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-04-18 01:27:55 -0400

    🗃 adds new env var

--- characters-service/src/config/dotenv.js
+++ characters-service/src/config/dotenv.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const DB_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/db';
+const DB_URI = process.env.MONGO_ATLAS || process.env.MONGO_URI;
 const PORT = process.env.PORT || 7100;
 
 export { DB_URI, PORT };
