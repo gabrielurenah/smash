1739b8c24b1777e876616314e24c02065eed42f2
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-05-01 04:05:13 -0400

    ✅ adds a simple test

--- players-service/test/unit/players.test.js
+++ players-service/test/unit/players.test.js
@@ -1,26 +1,7 @@
-import { Player } from '../../src/server/models/player';
-import { show } from '../../src/server/controllers/players';
-import { OK } from '../../src/config/statusCodes';
+import validateData from '#root/server/helpers/validateData';
 
-describe('PlayersHandlers', () => {
-  describe('show', () => {
-    afterEach(() => {
-      jest.restoreAllMocks();
-    });
-
-    it('should return a list of players', async () => {
-      console.log('Playeeerr', Player);
-      Player.findAll = jest.fn().mockResolvedValue([{ id: 1 }]);
-      const mockRes = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockResolvedValue(),
-      };
-      const result = await show({}, mockRes);
-
-      console.log('REEE', { result });
-
-      expect(mockRes.status).toHaveBeenCalledWith(OK);
-      expect(mockRes.json).toHaveBeenCalledWith([{ id: 1 }]);
-    });
+describe('validate data from request', () => {
+  it('should be defined', () => {
+    expect(validateData).toBeDefined();
   });
 });
